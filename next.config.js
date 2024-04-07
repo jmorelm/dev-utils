32cd7f9ffafd87f82a8304c04470531ac625db59
jmorelm <jmorelmarti@gmail.com>
2024-04-06 23:52:35 -0400

    next config fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/dev-utils",
-    output: 'standalone',
+    output: "export",
+    experimental: {
+        externalDir: true
+    },
     reactStrictMode: true,
 }
 
